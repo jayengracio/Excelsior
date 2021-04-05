f994a70ef9e35cab10b54f2dc8ff971564644468
John Engracio <johnengracio98@gmail.com>
2021-04-05 12:54:52 +0100

    optimizations to change gender (removed redundancies)

--- src/excelsior/Character.java
+++ src/excelsior/Character.java
@@ -191,17 +191,8 @@ public class Character extends ImageView {
             PixelReader reader = inputImage.getPixelReader();
             PixelWriter writer = outputImage.getPixelWriter();
 
-            int hairRed = (int) Math.round(defaultHairColour.getRed() * 255);
-            int hairGreen = (int) Math.round(defaultHairColour.getGreen() * 255);
-            int hairBlue = (int) Math.round(defaultHairColour.getBlue() * 255);
-
-            int newRed = (int) Math.round(hairColour.getRed() * 255);
-            int newGreen = (int) Math.round(hairColour.getGreen() * 255);
-            int newBlue = (int) Math.round(hairColour.getBlue() * 255);
-
             for (int y = 0; y < H; y++) {
                 for (int x = 0; x < W; x++) {
-
                     Color current = reader.getColor(x, y);
 
                     int curPixelRed = (int) Math.round(current.getRed() * 255);
@@ -220,59 +211,17 @@ public class Character extends ImageView {
                 }
             }
             return outputImage;
-        }
-        else
+        } else
             return this.character;
     }
 
     // Change gender helper function
     private void change(PixelWriter writer, int x, int y, Color current, Color newColour) {
-        if(current.equals(Color.web("#FBFF5E")) || current.equals(Color.web("#FDFC9E")))            //yellow exemptions
+        if (current.equals(Color.web("#FBFF5E")) || current.equals(Color.web("#FDFC9E")))          //yellow exemptions
             writer.setColor(x, y, current);
-        else if(current.equals(Color.web("#FF4D00")) || current.equals(Color.web("#FFBC89")))           //red exemptions
+        else if (current.equals(Color.web("#FF4D00")) || current.equals(Color.web("#FFBC89")))     //red exemptions
             writer.setColor(x, y, current);
         else
             writer.setColor(x, y, newColour);
     }
-
-
-
-    /*private Image changeGender(Image inputImage) {
-        int skinRed = (int) Math.round(skinColour.getRed() * 255);
-        int skinGreen = (int) Math.round(skinColour.getGreen() * 255);
-        int skinBlue = (int) Math.round(skinColour.getBlue() * 255);
-
-        Color bowtie = Color.web("#ECB4B5");
-        Color lipstick = Color.web("FF0000");
-
-        Color noFemaleHair = Color.web("#FEFEFE");
-        Color noBowtie = Color.web("#FDFDFD");
-        Color noLipstick = Color.rgb(skinRed, skinGreen, skinBlue);
-
-        int width = (int) inputImage.getWidth();
-        int height = (int) inputImage.getHeight();
-        WritableImage output = new WritableImage(width, height);
-        PixelReader reader = inputImage.getPixelReader();
-        PixelWriter writer = output.getPixelWriter();
-
-        for (int y = 0; y < width; y++) {
-            for (int x = 0; x < height; x++) {
-                if (!this.isFemale) {
-                    change(femaleHairColour, bowtie, lipstick, reader, writer, y, x, noFemaleHair, noBowtie, noLipstick);
-                } else {
-                    return this.character;
-                }
-            }
-        }
-        return output;
-    }
-
-    // Change gender helper function
-    private void change(Color noFemaleHair, Color noBowtie, Color noLipstick, PixelReader reader, PixelWriter writer,
-                        int y, int x, Color femaleHairColour, Color bowtie, Color lipstick) {
-        if (reader.getColor(y, x).equals(noFemaleHair)) writer.setColor(y, x, femaleHairColour);
-        else if (reader.getColor(y, x).equals(noBowtie)) writer.setColor(y, x, bowtie);
-        else if (reader.getColor(y, x).equals(noLipstick)) writer.setColor(y, x, lipstick);
-        else writer.setColor(y, x, reader.getColor(y, x));
-    }*/
 }
